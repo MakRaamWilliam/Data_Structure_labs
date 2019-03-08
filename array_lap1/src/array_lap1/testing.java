a8650bd4eae2af24a48665a3b60fac865d3c9a64
MakRaamWilliam <makrm.wilyam99@gmail.com>
2019-03-08 18:20:14 +0200

    sec

--- array_lap1/src/array_lap1/testing.java
+++ array_lap1/src/array_lap1/testing.java
@@ -1,5 +1,7 @@
 package array_lap1;
 
+import java.math.BigInteger;
+
 public class testing {
 
 	public static void main(String[] args) {
@@ -7,11 +9,21 @@ public class testing {
 	//	for(int i=0;i<6;i++) a[i]=i+1;
 	//	a[6]=5; a[7]=5;a[8]=7;a[9]=7;
 		
-	//	MySpecialArrayUtils k = new MySpecialArrayUtils();
+		MySpecialArrayUtils t = new MySpecialArrayUtils();
 //		k.moveValue(a,5);
-		for(int i=0;i<a.length;i++) System.out.println(a[i]);
+	//	for(int i=0;i<a.length;i++) System.out.println(a[i]);
 		//System.out.println(k.average(a));
 	//	for(int i=0;i<a.length;i++) System.out.println(a[i]);
-	}
-
-}
+	Fibb f = new Fibb();
+	f.Fibonacci();
+	/*	int [][]arr = new int [3][2]; 
+		int k=0; 
+		  for(int i=0; i<arr.length;i++) {
+		    	for(int j=0;j<arr[0].length;j++) {
+		arr[i][j]=k;k++; }}
+  int [][] arr2= t.transpose(arr);  	
+		    	
+	 for(int i2=0; i2<arr2.length;i2++) {
+    	for(int j2=0;j2<arr2[0].length;j2++) { System.out.println(arr2[i2][j2]); }}
+    */
+}}
