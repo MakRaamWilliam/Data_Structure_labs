bb82329be7bed5b4581cf00a2880b0ad03e06973
MakRaamWilliam <48126243+MakRaamWilliam@users.noreply.github.com>
2019-03-09 22:30:28 +0200

    Update MySpecialArrayUtils.java

--- array_lap1/src/array_lap1/MySpecialArrayUtils.java
+++ array_lap1/src/array_lap1/MySpecialArrayUtils.java
@@ -1,10 +1,12 @@
 package array_lap1;
- import java.util.Scanner;
+
+import java.math.BigInteger;
+import java.util.Scanner;
 
 public class MySpecialArrayUtils {
 
 	public static void reverse(int[] arr) {
-	if(arr.length==10)	return;
+	
 		for(int i =0;i<(arr.length)/2;i++) {
 			int temp=arr[i]; arr[i]=arr[arr.length-1-i]; arr[arr.length-1-i]=temp;
 		}
@@ -16,10 +18,13 @@ public class MySpecialArrayUtils {
 			 else if((arr[i]%2)!=0) a[1] +=arr[i];
 		 }
 	return a; }
+	 
 	 public static double average(int[] arr) {
 		 if(arr.length==0) return 0;
-		 double sum=0;
-		 for(int i = 0; i<arr.length;i++) sum +=arr[i];
+		 double sum=0;    						//use double to avoid int over flow
+		 for(int i = 0; i<arr.length;i++) {
+			 sum +=arr[i];
+		 }
 		
 	return (sum/arr.length); }
 	 
@@ -39,11 +44,11 @@ public class MySpecialArrayUtils {
 					} if(flag==0) return ; flag =0;
 			 }
 		 }
-		 
 	 }
+		 
 	  public static int [][] transpose(int [][]arr){
-		  int [][]a = new int [arr[0].length][arr.length];
-		    for(int i=0; i<arr.length;i++) {
+		  int [][]a = new int [arr[0].length][arr.length];    // to get the coulum number 
+		    for(int i=0; i<arr.length;i++) {     				//to get rows number
 		    	for(int j=0;j<arr[0].length;j++) {
 		    		a[j][i]=arr[i][j];
 		    	}
@@ -51,19 +56,26 @@ public class MySpecialArrayUtils {
 		  
 		 return a; 
 	  }
-	
-	public static void  Fibonacci (){
-	  Scanner ip = new Scanner(System.in);
-	  int num = ip.nextInt();
-	 long n1= 1; long n2 = 0; long sum = 1;
-	 if(num == 1) System.out.println(0);
-	 else if(num>1) {
-		 for(int i = 2; i<=num;i++) {
-			 sum = n1+n2; 
-			 n1=n2; n2= sum;
-		 } System.out.println(sum);
-	 }
-		
-		
-	}
+	  public static BigInteger Fibonacci (int num){   // bigitger will avoid int over flow
+		//  Scanner ip = new Scanner(System.in);
+	//	  int num = ip.nextInt();
+		  
+		 BigInteger n1= new BigInteger("1");
+		 BigInteger n2= new BigInteger("0");
+		 BigInteger sum= new BigInteger("1");
+
+		 
+		 if(num == 1) return BigInteger.valueOf(0);
+		 else if(num>1) {
+			 for(int i = 2; i<=num;i++) {
+				 sum = n1.add(n2);
+				 n1=n2; n2= sum;
+			 } 
+		 } return sum;
+			
+			
+		}
+	 
+	 
+	 
 }
