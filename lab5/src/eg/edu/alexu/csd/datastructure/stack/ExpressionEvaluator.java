75214b54d2dee5475016082392028042dc162586
MakRaamWilliam <makrm.wilyam99@gmail.com>
2019-04-25 20:02:55 +0200

    test sheet

--- lab5/src/eg/edu/alexu/csd/datastructure/stack/ExpressionEvaluator.java
+++ lab5/src/eg/edu/alexu/csd/datastructure/stack/ExpressionEvaluator.java
@@ -12,23 +12,29 @@ public class ExpressionEvaluator implements IExpressionEvaluator {
 	}
 	
 	 boolean isdigit(char a) {
-		 if(a== '*' || a== '/' || a=='/' || a=='%' || a=='+' || a== '-' ) return false;
+		 if(a== '*' || a== '/' || a=='/' || a=='%' || a=='+' || a== '-' || a== ' ') return false;
 		 return true;
 	 }
 	
 	
 	public String infixToPostfix(String expression) {
-		Stack s = new Stack(); String op = ""; boolean flag=true;
+		Stack s = new Stack(); String op = ""; 
+		
 		for(int i =0; i< expression.length();i++) {
 			char a = expression.charAt(i);
 		if ( (a== ' ' || a==',' || a=='$' || a=='&' ) == false) {
 			if(priority(a) == 0) op = op + a ;
 			else {  op = op+ " ";
-				if(s.isEmpty() ||( ( char)s.peek() == '(' ) || (a == '(' ))  s.push((char) a)  ;
+			 if ( a == '-' && i < expression.length() -1 && isdigit( expression.charAt(i+1)) ) {
+				op = op+ "-";
+			}
+			else if(  s.isEmpty()  ||( ( char)s.peek() == '(' ) || (a == '(' ) ) 
+				s.push((char) a)  ;
 				else if (a== ')' ) {
-					while( (char)s.peek() != '(') op =  op +(char) s.pop() + " ";
+					while( (char)s.peek() != '(') op =  op +(char) s.pop() ;
 					s.pop();
-				} else {
+				} 
+				else { 
 					while( !s.isEmpty() && priority(a) <= priority( (char) s.peek() )  ) {
 						op = op + (char) s.pop() + " ";
 					} s.push( (char ) a);
@@ -36,35 +42,51 @@ public class ExpressionEvaluator implements IExpressionEvaluator {
 				
 			}
 			
-		}	} while (!s.isEmpty()) op=op+ (char) s.pop() + " ";
+		}	} 
+		if( ( priority(expression.charAt(expression.length()-1)) == 0 ) && !s.isEmpty()) op = op + " "; 
+		if(op.charAt(op.length()-1) != ' ' && !s.isEmpty()) op = op+ " ";
+		while (!s.isEmpty() && !s.isEmpty()) op=op+ (char) s.pop() + " ";
 		
-		op= op.substring(0, op.length()-1);
+		op= op.substring(0, op.length());
 		while (op.charAt(0) == ' ') op= op.substring(1);
-		return op;
+		String OP2 =""; int i=0;
+		while( i< op.length()) {
+			char a = op.charAt(i);
+			OP2 +=a;
+			if(a== ' ') {
+				while ( (i+1)< op.length() &&  op.charAt(i+1)==' ' ) i++;
+			} 
+			
+	i++;	} 
+		if(OP2.charAt(OP2.length()-1) == ' ') OP2= OP2.substring(0,OP2.length()-1);
+		return OP2;
 	}
 
-	@Override
+	
 	public int evaluate(String expression) {
 		Stack s = new Stack(); String dig= "";
 		for(int i =0 ; i< expression.length() ;i++) {
 			char a = expression.charAt(i);
 			if (a != ' ' &&  isdigit(a)) {
-		//	if(isdigit(a)) s.push((int ) a -48);  
+		
 			dig=dig+a;	
-			} else {  
-	if(dig.length() !=0)	s.push(  (int) Integer.parseInt(dig) ); dig = "";
+			} else if(a == '-' && i < expression.length() -1 && isdigit( expression.charAt(i+1) ) ) {
+				dig = dig+ "-";
+			}
+			else {  
+	if(dig.length() !=0)	s.push(  (float) Float.parseFloat(dig) ); dig = "";
 			if(a != ' ') {
 			 if(s.size()<2) new  RuntimeException("Erorr expression");
-				int x=  (int) s.pop(); int y =  (int) s.pop();
-				if(a == '*') s.push( (int) ( y*x) );
-				else if(a== '/' ) s.push( (int) ( y/x )  );
-				else if (a== '%') s.push( (int) ( y%x )  );
-				else if(a== '+' ) s.push( (int) ( y+x)  );
-				else if(a== '-') s.push( (int) ( y-x )  );
+				float x=  (float) s.pop(); float y =  (float) s.pop();
+				if(a == '*') s.push( (float) ( y*x) );
+				else if(a== '/' ) s.push( (float) ( y/x )  );
+				else if (a== '%') s.push( (float) ( y%x )  );
+				else if(a== '+' ) s.push( (float) ( y+x)  );
+				else if(a== '-') s.push( (float) ( y-x )  );
 			
 			} }	}
-		
-		return (int) s.pop();
+		   float sum = (float) s.pop();
+		return Math.round(sum);
 	}
 	
 
--- lab5/src/eg/edu/alexu/csd/datastructure/stack/stackTest.java
+++ lab5/src/eg/edu/alexu/csd/datastructure/stack/stackTest.java
@@ -14,14 +14,14 @@ class stackTest {
 		test.push((int) 5) ; test.push((int) 10);
 		assertEquals(10, test.peek()); assertEquals(10, test.pop());
 		
-		
+		// there  tests are  in the sheet with the answers 
 	       ExpressionEvaluator ev =  new ExpressionEvaluator();
-	       assertEquals("2 3 4 * + " , ev.infixToPostfix("2 + 3 * 4"));
-	       assertEquals("a b * 5 + " , ev.infixToPostfix("a * b + 5"));
-	       assertEquals("1 2 + 7 * " , ev.infixToPostfix("(1 + 2) * 7"));
-	       assertEquals("a b * c / " , ev.infixToPostfix("a * b / c"));
-	       assertEquals("a b c - d + / e a - * c * " , ev.infixToPostfix("(a / (b - c + d)) * (e - a) * c"));
-	       assertEquals("a b / c - d e * + a c * - " , ev.infixToPostfix("a / b - c + d * e - a * c"));
+	       assertEquals("2 3 4 * +" , ev.infixToPostfix("2 + 3 * 4"));
+	       assertEquals("a b * 5 +" , ev.infixToPostfix("a * b + 5"));
+	       assertEquals("1 2 + 7 *" , ev.infixToPostfix("(1 + 2) * 7"));
+	       assertEquals("a b * c /" , ev.infixToPostfix("a * b / c"));
+	       assertEquals("a b c - d + / e a - * c *" , ev.infixToPostfix("(a / (b - c + d)) * (e - a) * c "));
+	       assertEquals("a b / c - d e * + a c * -" , ev.infixToPostfix("a / b - c + d * e - a * c"));
 	       
 	}
 
