2e8e0816b8f830014ca3a79bd1479456ca26d9a1
MakRaamWilliam <makrm.wilyam99@gmail.com>
2019-04-27 14:52:00 +0200

    Junit Tests

---
+++ lab5/src/eg/edu/alexu/csd/datastructure/stack68/stackTest.java
@@ -0,0 +1,44 @@
+package eg.edu.alexu.csd.datastructure.stack68;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class stackTest {
+
+	@Test
+	void test() {
+		Stack test = new Stack();
+		assertEquals(test.isEmpty() , true);
+		assertEquals((int) 0, test.size());
+		test.push((int) 5) ; test.push((int) 10);
+		assertEquals(10, test.peek());
+		assertEquals(10, test.pop());
+		
+		// there  tests are  in the sheet with the answers 
+	       ExpressionEvaluator ev =  new ExpressionEvaluator();
+	       assertEquals("2 3 4 * +" , ev.infixToPostfix("2 + 3 * 4"));
+	       assertEquals("a b * 5 +" , ev.infixToPostfix("a * b + 5"));
+	       assertEquals("1 2 + 7 *" , ev.infixToPostfix("(1 + 2) * 7"));
+	       assertEquals("a b * c /" , ev.infixToPostfix("a * b / c"));
+	       assertEquals("a b c - d + / e a - * c *" , ev.infixToPostfix("(a / (b - c + d)) * (e - a) * c "));
+	       assertEquals("a b / c - d e * + a c * -" , ev.infixToPostfix("a / b - c + d * e - a * c"));
+	     
+	       
+	       //lecture example 
+	       assertEquals("3 1 4 6 + + * 2 + 8 + 5 * 4 7 2 + * +", ev.infixToPostfix("(3 * (1 + (4+6))+2+8)*5+4*(7+2)") );
+	       assertEquals(251, ev.evaluate ( ev.infixToPostfix("(3 * (1 + (4+6))+2+8)*5+4*(7+2)") ) );
+	       
+	       
+	       // tests for negative  & more one digit number 
+	      assertEquals(17, ev.evaluate (ev.infixToPostfix( "-3 +4 * 5" ) ));				// check negative number
+	      assertEquals(-60, ev.evaluate (ev.infixToPostfix( "(4 + 2) * -10" ) ));				// check negative number
+
+	      
+	    //check more  digit & floating result during evaluate 
+	      assertEquals(72, ev.evaluate (ev.infixToPostfix( "(35 + 2) + 7 / 2 * 10" ) ));
+	      
+	      
+	       
+	       }
+}
